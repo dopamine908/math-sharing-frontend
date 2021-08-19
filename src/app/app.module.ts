c65e94684c6c68a520bdfd063c2a7b51cb22be1d
sam <m3gl4a@gmail.com>
2021-08-19 23:54:28 +0800

    add module ngx-translate, extract-math, ng-katex and tailwindcss to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
+import { KatexModule } from 'ng-katex';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    KatexModule,
+    TranslateModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
