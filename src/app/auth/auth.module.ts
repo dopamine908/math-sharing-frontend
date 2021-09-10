36ba1eb72a9319456b5fca7c7f18afcc41108f16
samjheng <s12113120@stu.edu.tw>
2021-09-10 17:26:21 +0800

    feat:新增登入頁面

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class AuthModule { }
