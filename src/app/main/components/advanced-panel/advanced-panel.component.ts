876728b91ca6d777b62f859c21e805741b7f8934
sam.jheng <sam.jheng@ulictek.com>
2021-11-20 17:40:38 +0800

    feat:新增遞迴樹狀adv 搜尋條件

--- src/app/main/components/advanced-panel/advanced-panel.component.ts
+++ src/app/main/components/advanced-panel/advanced-panel.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced-panel.component.scss']
 })
 export class AdvancedPanelComponent implements OnInit {
-
+  public list: AdvancedModel[]=[
+    {
+      name: "國中數學",
+      children: []
+    },
+    {
+      name:"高中數學",
+      children:[]
+    },
+    {
+      name: "負數",
+      children: [{
+        name: "負數的意義"
+      }]
+    },
+    {
+      name: "數線與幾何",
+      children: [{
+        name: "幾何學",
+        children:[
+          {
+            name:"畢氏定理"
+          }
+        ]
+      }]
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+class AdvancedModel {
+  public name!: string;
+  public children?: AdvancedModel[];
 }
